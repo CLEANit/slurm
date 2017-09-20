4dd6f12da54ad27b2b82457d2852f20513e59ee0
Kyle Mills <kyle@kylemills.net>
2017-09-20 11:33:59 -0400

    added user stuff

---
+++ configuration/users/chuid_all.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo bash chuid.sh primus 1510 2510
+sudo bash chuid.sh kmills 1511 2511
+sudo bash chuid.sh itamblyn 1512 2512
+sudo bash chuid.sh kryczko 1513 2513
+sudo bash chuid.sh cbeeler 1514 2514
+sudo bash chuid.sh sgholami 1515 2515
+sudo bash chuid.sh iluchak 1516 2516
+sudo bash chuid.sh adomurad 1517 2517

441f9aa10fe859445fcfe14eecc567fb41b8f7e6
Kyle Mills <kyle.mills@uoit.net>
2017-09-25 11:41:18 -0400

    endess loop removed

--- configuration/monitor/head_node_running.sh
+++ configuration/monitor/head_node_running.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 
-while true; do
-    sleep 1
-
-
 pids_to_check=$(nvidia-smi  | grep -A 10000 PID | grep -v '+-------' | awk 'NR > 2{print $3}')
 #pids_to_check=$(pgrep python)$pids_to_check
 
-
-non_slurm_pids=$(comm -23 <(echo $pids_to_check) <(scontrol listpids 2>/dev/null | awk 'NR>1 {print $1}'))
+if [[ ! "x$pids_to_check" == "x" ]] ;
+then
+    non_slurm_pids=$(comm -23 <(echo $pids_to_check) <(scontrol listpids 2>/dev/null | awk 'NR>1 {print $1}'))
+else
+    non_slurm_pids=""
+fi
 
 echo "Processes I'm keeping an eye on:"
 for pid in $non_slurm_pids; 
@@ -24,7 +24,7 @@ for pid in $non_slurm_pids;
 do 
     etime=$(ps -o etimes= $pid)
     if [[ $etime -gt 60 ]]; then
-        echo "PID $pid has been running for over 1 minutes. Sending SIGINT."
+        echo "PID $pid has been running for over 1 minute. Sending SIGINT."
         kill -s SIGINT $pid
     fi
 
@@ -32,7 +32,5 @@ do
         echo "PID $pid has been running for over 10 minutes and has ignored the previously sent SIGINTs. Sending SIGKILL."
         kill -s SIGKILL $pid
     fi
-done
-
 done
 

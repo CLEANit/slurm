32fba9b22e02a5e5657053caf8624adc26c09208
Kyle Mills <kyle.mills@uoit.net>
2017-09-25 13:38:22 -0400

    incorrectly identifying processes to kill

--- configuration/monitor/head_node_running.sh
+++ configuration/monitor/head_node_running.sh
@@ -1,18 +1,30 @@
 #!/bin/bash
 
 
-pids_to_check=$(nvidia-smi  | grep -A 10000 PID | grep -v '+-------' | awk 'NR > 2{print $3}' | grep  -v 'running')
-#pids_to_check=$(pgrep python)$pids_to_check
+GPU_PIDs=$(nvidia-smi  | grep -A 10000 PID | grep -v '+-------' | awk 'NR > 2{print $3}' | grep  -v 'running' | sort )
 
 
-if [[ ! "x$pids_to_check" == "x" ]] ;
+if [[ ! "x$GPU_PIDs" == "x" ]] ;
 then
-    echo "Checking to see if $pids_to_check are slurm PIDs"
-    non_slurm_pids=$(comm -23 <(echo $pids_to_check) <(scontrol listpids 2>/dev/null | awk 'NR>1 {print $1}'))
+    slurm_PIDs=$(scontrol listpids 2>/dev/null | awk 'NR>1 {print $1}' | sort)
+    echo "GPU PIDs:"
+    echo $GPU_PIDs
+    echo ""
+    echo "SLURM PIDs:"
+    echo $slurm_PIDs
+    echo ""
+
+    echo "GPU PIDs that are not SLURM PIDs:"
+    non_slurm_pids=$(diff -U 10000  <(for i in $(echo $GPU_PIDs); do echo $i; done ) <(for j in $(echo $slurm_PIDs); do echo $j; done )  | sed -n 's/^-//p' | awk 'NR>1{print $1}')
+    echo $non_slurm_pids
+
 else
     echo "No processes to check"
     non_slurm_pids=""
 fi
+
+
+
 
 if [[ ! "x$non_slurm_pids" == "x" ]] ;
 then
@@ -29,7 +41,7 @@ then
         etime=$(ps -o etimes= $pid)
         if [[ $etime -gt 600 ]]; then
             echo "PID $pid has been running for over 10 minutes and has ignored both SIGINTs and SIGTERMs. Sending SIGKILL."
-            kill -s SIGKILL $pid
+            kill -s SIGKILLi $pid
         elif [[ $etime -gt 300 ]]; then
             echo "PID $pid has been running for over 5 minutes and has ignored the previous SIGINTs. Sending SIGTERM."
             kill -s SIGTERM $pid

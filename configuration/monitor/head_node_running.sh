cf64880f44c00dbe400fd7f56e2266aae76a8025
Kyle Mills <kyle.mills@uoit.net>
2017-09-25 11:12:36 -0400

    update for GPU

--- configuration/monitor/head_node_running.sh
+++ configuration/monitor/head_node_running.sh
@@ -6,7 +6,7 @@ while true; do
 
 
 pids_to_check=$(nvidia-smi  | grep -A 10000 PID | grep -v '+-------' | awk 'NR > 2{print $3}')
-pids_to_check=$(pgrep python)$pids_to_check
+#pids_to_check=$(pgrep python)$pids_to_check
 
 
 non_slurm_pids=$(comm -23 <(echo $pids_to_check) <(scontrol listpids 2>/dev/null | awk 'NR>1 {print $1}'))
@@ -23,13 +23,13 @@ done
 for pid in $non_slurm_pids; 
 do 
     etime=$(ps -o etimes= $pid)
-    if [[ $etime -gt 600 ]]; then
-        echo "PID $pid has been running for over 10 minutes. Sending SIGINT."
+    if [[ $etime -gt 60 ]]; then
+        echo "PID $pid has been running for over 1 minutes. Sending SIGINT."
         kill -s SIGINT $pid
     fi
 
-    if [[ $etime -gt 900 ]]; then
-        echo "PID $pid has been running for over 15 minutes and has ignored the previously sent SIGINTs. Sending SIGKILL."
+    if [[ $etime -gt 600 ]]; then
+        echo "PID $pid has been running for over 10 minutes and has ignored the previously sent SIGINTs. Sending SIGKILL."
         kill -s SIGKILL $pid
     fi
 done

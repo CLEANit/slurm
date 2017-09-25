f840af975fa057decec9d52d4b575e002d286c74
Kyle Mills <kyle.mills@uoit.net>
2017-09-25 13:00:54 -0400

    fixed issue with head node checker

--- configuration/monitor/head_node_running.sh
+++ configuration/monitor/head_node_running.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
 
-pids_to_check=$(nvidia-smi  | grep -A 10000 PID | grep -v '+-------' | awk 'NR > 2{print $3}')
+pids_to_check=$(nvidia-smi  | grep -A 10000 PID | grep -v '+-------' | awk 'NR > 2{print $3}' | grep  -v 'running')
 #pids_to_check=$(pgrep python)$pids_to_check
+
 
 if [[ ! "x$pids_to_check" == "x" ]] ;
 then
+    echo "Checking to see if $pids_to_check are slurm PIDs"
     non_slurm_pids=$(comm -23 <(echo $pids_to_check) <(scontrol listpids 2>/dev/null | awk 'NR>1 {print $1}'))
 else
+    echo "No processes to check"
     non_slurm_pids=""
 fi
 
 if [[ ! "x$non_slurm_pids" == "x" ]] ;
 then
 
-    echo "Processes I'm keeping an eye on:"
+    echo "Processes I'm keeping an eye on: $non_slurm_pids"
     for pid in $non_slurm_pids; 
     do 
         echo -e $pid "\t" $(ps -o etimes= $pid)
@@ -30,8 +33,8 @@ then
         elif [[ $etime -gt 300 ]]; then
             echo "PID $pid has been running for over 5 minutes and has ignored the previous SIGINTs. Sending SIGTERM."
             kill -s SIGTERM $pid
-        elif [[ $etime -gt 60 ]]; then
-            echo "PID $pid has been running for over 1 minute. Sending SIGINT."
+        elif [[ $etime -gt 5 ]]; then
+            echo "PID $pid has been running for over 5 seconds. Sending SIGINT."
             kill -s SIGINT $pid
         fi
 
